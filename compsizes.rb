4cfcf5d106545ba306623e1cc4a157bf75a41217
Christopher James Huff <cjameshuff@gmail.com>
2011-10-02 18:37:06 -0400

    removed LPC define from GCC commands

--- compsizes.rb
+++ compsizes.rb
@@ -2,9 +2,9 @@
 
 GCC = 'arm-none-eabi-gcc'
 
-`#{GCC} -mthumb -mcpu=cortex-m3 -DPART_lpc1343 -ffunction-sections -fdata-sections -MD -std=gnu99 -Wall -Os -c uformat.c -o uformat.c.o`
+`#{GCC} -mthumb -mcpu=cortex-m3 -ffunction-sections -fdata-sections -MD -std=gnu99 -Wall -Os -c uformat.c -o uformat.c.o`
 `arm-none-eabi-objcopy -O binary uformat.c.o uformat_large.bin`
-`#{GCC} -mthumb -mcpu=cortex-m3 -DPART_lpc1343 -DUPRINTF_TINY -ffunction-sections -fdata-sections -MD -std=gnu99 -Wall -Os -c uformat.c -o uformat.c.o`
+`#{GCC} -mthumb -mcpu=cortex-m3 -DUPRINTF_TINY -ffunction-sections -fdata-sections -MD -std=gnu99 -Wall -Os -c uformat.c -o uformat.c.o`
 `arm-none-eabi-objcopy -O binary uformat.c.o uformat_tiny.bin`
 puts "Full: #{File.size('uformat_large.bin')} B"
 puts "Tiny: #{File.size('uformat_tiny.bin')} B"
